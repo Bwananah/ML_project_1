dde065a5d5dea9e874d59ac72147f0fda7da57c9
Ewan Golfier <ewan.golfier@epfl.ch>
2022-10-20 17:07:44 +0200

    Results for GD, SGD, LS and Ridge

--- helpers.py
+++ helpers.py
@@ -36,3 +36,89 @@ def create_csv_submission(ids, y_pred, name):
         writer.writeheader()
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({"Id": int(r1), "Prediction": int(r2)})
+
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x)
+    x = x - mean_x
+    std_x = np.std(x)
+    x = x / std_x
+    return x, mean_x, std_x
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8 
+    you will have 80% of your data set dedicated to training 
+    and the rest dedicated to testing. If ratio times the number of samples is not round
+    you can use np.floor. Also check the documentation for np.random.permutation,
+    it could be useful.
+    
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        y: numpy array of shape (N,).
+        ratio: scalar in [0,1]
+        seed: integer.
+        
+    Returns:
+        x_tr: numpy array containing the train data.
+        x_te: numpy array containing the test data.
+        y_tr: numpy array containing the train labels.
+        y_te: numpy array containing the test labels.
+        
+    >>> split_data(np.arange(13), np.arange(13), 0.8, 1)
+    (array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]), array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]))
+    """
+    # set seed
+    np.random.seed(seed)
+    
+    n = y.shape[0]
+    nb_tr = int(np.floor(n * ratio))
+    
+    indices = np.random.permutation(np.arange(n))
+    
+    x_tr = x[indices[:nb_tr]]
+    x_te = x[indices[nb_tr:]]
+    y_tr = y[indices[:nb_tr]]
+    y_te = y[indices[nb_tr:]]
+    
+    return x_tr, x_te, y_tr, y_te
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree.
+    
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        degree: integer.
+        
+    Returns:
+        poly: numpy array of shape (N,d+1)
+        
+    >>> build_poly(np.array([0.0, 1.5]), 2)
+    array([[1.  , 0.  , 0.  ],
+           [1.  , 1.5 , 2.25]])
+    """
+    n = x.shape[0]
+    res = np.zeros((n, degree+1))
+    
+    for i in range(n):
+        for j in range(degree+1):
+            res[i, j] = x[i]**j
+            
+    return res
+
+def compute_loss(y, tx, w):
+
+    """Calculate the loss using MSE
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N, M)
+        w: numpy array of shape=(M,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    e = y - tx@w
+    n = y.shape[0]
+    
+    return e.T@e/(2*n)
